db427d05b66ee6e675db61aa3574b2c0d04be06d
hackmitel <none>
2019-05-18 19:50:22 -0700

    initial payload for Cocoa Touch Framework

---
+++ HCKalmanFilter/HCKalmanFilter.h
@@ -0,0 +1,19 @@
+//
+//  HCKalmanFilter.h
+//  HCKalmanFilter
+//
+//  Created by klebs_osx on 5/18/19.
+//  Copyright © 2019 klebs_osx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HCKalmanFilter.
+FOUNDATION_EXPORT double HCKalmanFilterVersionNumber;
+
+//! Project version string for HCKalmanFilter.
+FOUNDATION_EXPORT const unsigned char HCKalmanFilterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HCKalmanFilter/PublicHeader.h>
+
+
